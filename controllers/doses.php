a5c15737a2e9ee66e6c15e7682221b69e5c3f523
Rodrigo Polastro <rodrigo.polastro03@gmail.com>
2024-06-15 01:06:47 -0300

    feat: (completo) aumenta um dia na data de doses quando passam da meia-noite

--- controllers/doses.php
+++ controllers/doses.php
@@ -29,26 +29,26 @@ function controllerDoses($action)
                 $_GET['treatment_start_date'],
                 new DateTimeZone('America/Sao_Paulo')
             );
-            $total_usage_days = $_GET['total_usage_days'];
-
+            
             // what we call "today's midnight" actually belongs to "tomorrow"
-            $days_increment = $_GET['dose_time_1'] == '00:00' ? 1 : 0; 
-
-            for ($days_increment = 0; $days_increment < $total_usage_days; $days_increment++) {
+            $days_increment = $_GET['dose_time_1'] == '00:00' ? 1 : 0;
+            
+            for ($days_increment; $days_increment < $$_GET['total_usage_days']; $days_increment++) {
                 $increment_string = 'P' . $days_increment . 'D';
-                $dose_due_date = $initial_date_time->add(new DateInterval($increment_string))->format('Y-m-d');
+                $dose_due_date = $initial_date_time->add(new DateInterval($increment_string));
                 for ($dose_time_index = 1; $dose_time_index <= $_GET['doses_per_day']; $dose_time_index++) {
                     $dose_due_time = $_GET['dose_time_' . $dose_time_index];
+                    $dose_hours = explode(':', $dose_due_time)[0];
+                    $first_dose_hours = explode(':', $_GET['dose_time_1'])[0];
 
-                    // increase one day if the dose time has overflowed
-                    if ($dose_due_time < $_GET['dose_time_1']) {
-                        $dose_due_date++;
+                    if (intval($dose_hours) < intval($first_dose_hours)) {
+                        $dose_due_date = $dose_due_date->add(new DateInterval('P1D'));
                     }
 
                     $dose = [
                         'medicine_id' => $_GET['medicine_id'],
-                        'due_date' =>    $dose_due_date,
-                        'due_time' =>    $dose_due_time,
+                        'due_date' => $dose_due_date->format('Y-m-d'),
+                        'due_time' => $dose_due_time,
                     ];
                     foreach ($dose as $key => $value) {
                         if (empty($value)) {
